76ef6cc4c718864a8f3aa5407bdcff1b19ec1d01
Rafal <kurrenda@gmail.com>
2019-05-22 18:53:36 +0200

    Poprawione algorytmy

--- Data.py
+++ Data.py
@@ -3,10 +3,9 @@ import csv
 
 class Count:
 
-    def __init__(self,filename, year, province):
+    def __init__(self,filename):
         self.filename = filename
-        self.year = year
-        self.province = province
+
 
     def average(self):
         with open(self.filename, 'r', encoding='windows-1250') as csvfile:
@@ -22,22 +21,23 @@ class Count:
                                 peopleInt = int(''.join(map(str, row['Liczba osób'])))
                                 counter += 1
                                 count += peopleInt
-                result = count / counter
-                print(self.year, "-", round(result), end='\n')
+                if counter != 0:
+                    result = count / counter
+                    print(self.year, "-", round(result))
             else:
                 print("Podałeś zły rok lub złą nazwę województwa(zakres 2010-2018)")
 
 
-    def percent(self):
+    def percent(self,year,province):
         with open(self.filename, 'r', encoding='windows-1250') as csvfile:
             document = csv.DictReader(csvfile, delimiter=';')
             countAll = 0
             countPassed = 0
-            if self.year <= 2018 and self.year >= 2010:
+            if year <= 2018 and year >= 2010:
                 for row in document:
-                    if self.province == row['Terytorium'] and self.province != 'Polska':
+                    if province == row['Terytorium'] and province != 'Polska':
                         yearInt = int(''.join(map(str, row['Rok'])))
-                        if yearInt <= self.year:
+                        if year == yearInt:
                             if row['Przystąpiło/zdało '] == 'przystąpiło':
                                 peopleIntAll = int(''.join(map(str, row['Liczba osób'])))
                                 countAll += peopleIntAll
@@ -45,9 +45,45 @@ class Count:
                                 peopleIntPassed = int(''.join(map(str, row['Liczba osób'])))
                                 countPassed += peopleIntPassed
 
+
                 if(countAll != 0):
-                    result = (countPassed/countAll) * 100.0
-                    print(self.year,"-",round(result))
-            else:
-                print("Podałeś zły rok lub złą nazwę województwa(zakres 2010-2018)")
+                    result = int((countPassed/countAll) * 100.0)
+                    return result
+            return 0
+
+    def bestPass(self, year):
+        with open(self.filename, 'r', encoding='windows-1250') as csvfile:
+            document = csv.DictReader(csvfile, delimiter=';')
+            result = 0
+            if year <= 2018 and year >= 2010:
+                for row in document:
+                    province = row['Terytorium']
+                    a = self.percent(year, province)
+                    if a > result:
+                        provinceResult = province
+                        result = a
+                print(provinceResult,result)
+
+
+d = Count('dane.csv')
+print(d.percent(2013, 'Małopolskie'))
+d.bestPass(2013)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
